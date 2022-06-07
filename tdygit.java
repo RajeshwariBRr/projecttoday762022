11b55856ade8bad00dd682a8987a2318b67615a3
RajeshwariBRr <rajeshwari.raj78@gmail.com>
2022-06-07 22:30:43 +0530

    Project structure

---
+++ tdygit.java
@@ -0,0 +1,28 @@
+package projecttoday76202022;
+
+import org.testng.annotations.Test;
+
+public class tdygit {
+	public class C {
+
+		@Test		
+		private void testC1() {
+			System.out.println("TestC2");	
+			System.out.println(Thread.currentThread().getId());
+	}
+	@Test		
+	private void testC2() {
+		System.out.println("TestC2");	
+		System.out.println(Thread.currentThread().getId());
+				
+	}
+	@Test
+		private void testC3() {
+			System.out.println("TestC3");	
+			System.out.println(Thread.currentThread().getId());
+				
+				
+		}			
+				
+}
+}
